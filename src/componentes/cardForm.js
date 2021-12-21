eb2eb2de874ba69c37054a4c4c491d82ff15ed3f
Phelipe <phelipe662@gmail.com>
2021-12-21 03:28:49 -0300

    erros no form, utils

--- src/componentes/cardForm.js
+++ src/componentes/cardForm.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import {View, StyleSheet, TextInput} from 'react-native';
+import {formatStringToPtBrLocaleValueFormat} from '../utils/utils';
 
-const CardForm = (props) => {
-    const { acaoValorFormatado, accessibilityLabel, onPress, index } = props.item;
+const CardForm = props => {
+  const {acaoValorFormatado, accessibilityLabel, onPress, index, value} = props.item;
 
-    const styles = StyleSheet.create({
-        container: {
-            backgroundColor: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-            padding: 8,
-            marginVertical: 0.5,
-            flexDirection: 'row',
-        }
-    });
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 8,
+      marginVertical: 0.5,
+      flexDirection: 'row',
+    },
+  });
 
-    return (
-        <View style={styles.container}>
-            <TextInput
-                style={{ fontSize: 17, textAlign: 'left', flex: 1 }}
-                onChangeText={(value) => onPress(value, index, acaoValorFormatado)}
-                accessibilityLabel={accessibilityLabel}
-                keyboardType="numeric"
-            />
-        </View>
-    );
-}
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={{fontSize: 17, textAlign: 'left', flex: 1}}
+        value={`R$ ${formatStringToPtBrLocaleValueFormat(value)}`}
+        onChangeText={value => onPress(value, index, acaoValorFormatado)}
+        accessibilityLabel={accessibilityLabel}
+        keyboardType="numeric"
+      />
+    </View>
+  );
+};
 
 export default CardForm;
--- src/services/investimentoService.js
+++ src/services/investimentoService.js
@@ -1,16 +1,16 @@
-import axios from "axios";
-import { API_INVESTIMENTOS } from "../utils/constantes";
+import axios from 'axios';
+import {API_INVESTIMENTOS} from '../utils/constantes';
 
 export default class InvestimentoService {
-    async getInvestimentoRequest() {
-        return new Promise(async (resolve, reject) => {
-            const url = API_INVESTIMENTOS;
-            try {
-                const response = await axios.get(url);
-                resolve(response.data.response.data.listaInvestimentos);
-            } catch (error) {
-                reject(new Error('Ocorreu um erro ao recuperar a lista de investimentos'))
-            }
-        })
-    }
+  async getInvestimentoRequest() {
+    return new Promise(async (resolve, reject) => {
+      const url = API_INVESTIMENTOS;
+      try {
+        const response = await axios.get(url);
+        resolve(response.data.response.data.listaInvestimentos);
+      } catch (error) {
+        console.log('oi');
+      }
+    });
+  }
 }
